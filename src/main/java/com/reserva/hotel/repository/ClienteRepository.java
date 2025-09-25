efbf42e40eae7d7768ed4918a0797f5f6f0c0761
Natacha Duarte <natachadpduarte@gmail.com>
2025-09-25 00:49:59 -0300

    feat: Criando respository's

--- src/main/java/com/reserva/hotel/repository/ClienteRepository.java
+++ src/main/java/com/reserva/hotel/repository/ClienteRepository.java
@@ -1,9 +1,14 @@
 package com.reserva.hotel.repository;
 
-import com.reserva.hotel.model.CadastroCliente;
+import com.reserva.hotel.model.ClienteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ClienteRepository extends JpaRepository<CadastroCliente, Long> {
+public interface ClienteRepository extends JpaRepository<ClienteEntity, Long> {
+
+    Optional<ClienteEntity> findByDocumentoOrPassaporte(String documento, String passaporte);
+
 }
--- src/main/java/com/reserva/hotel/repository/QuartoRepository.java
+++ src/main/java/com/reserva/hotel/repository/QuartoRepository.java
@@ -1,10 +1,15 @@
 package com.reserva.hotel.repository;
 
-import com.reserva.hotel.model.CadastroQuarto;
+import com.reserva.hotel.model.QuartoEntity;
+import com.reserva.hotel.utils.enums.NomeQuartoEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface QuartoRepository extends JpaRepository<CadastroQuarto, Long> {
+import java.util.List;
+import java.util.Optional;
 
+@Repository
+public interface QuartoRepository extends JpaRepository<QuartoEntity, Long> {
+
+    QuartoEntity findByNomeQuarto (NomeQuartoEnum nomeQuarto);
 }
